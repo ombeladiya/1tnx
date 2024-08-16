0e70d90684ec2419d4dcdc9c21b5634fe9a61726
ombeladiya <ombeladiya1854@gmail.com>
2024-08-16 18:37:29 +0530

    video added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-full min-h-screen w-full items-center justify-between">
-      <img
+    <main className="flex bg-black flex-col h-full min-h-screen w-full items-center justify-between">
+      {/* <img
         src="/commingsoon.jpg"
         alt="comming soon"
         className="h-full min-h-screen w-full"
-      />
+      /> */}
+
+      <video preload="auto" loop muted className="w-screen h-screen" autoPlay playsInline>
+        <source src="/home.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
     </main>
   );
 }
