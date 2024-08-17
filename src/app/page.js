9ca98183d71d8c51bf10df0453df89cd6e84e914
ombeladiya <ombeladiya1854@gmail.com>
2024-08-17 10:22:19 +0530

    x lik added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
-
+"use client"
 export default function Home() {
+  const handleX = () => {
+    window.location = 'https://x.com/onetnx?t=XvwO4e2xRh_bBygRDZpj0A&s=09'
+  }
+
   return (
     <main className="flex bg-black flex-col h-full min-h-screen w-full items-center justify-between">
       {/* <img
@@ -8,7 +12,7 @@ export default function Home() {
         className="h-full min-h-screen w-full"
       /> */}
 
-      <video preload="auto" loop muted className="w-screen h-screen" autoPlay playsInline>
+      <video preload="auto" loop muted className="w-screen h-screen cursor-pointer" autoPlay playsInline onClick={handleX}>
         <source src="/home.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
